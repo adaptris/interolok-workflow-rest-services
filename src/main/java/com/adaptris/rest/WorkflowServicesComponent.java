04199237bd3c4963e0b0c59b3d0da546af13c997
Din <din.stolak@proagrica.com>
2019-01-29 10:45:37 +0000

    INTERLOK-2372 Updated workflow services to filter and return workflow

--- src/main/java/com/adaptris/rest/WorkflowServicesComponent.java
+++ src/main/java/com/adaptris/rest/WorkflowServicesComponent.java
@@ -2,7 +2,8 @@ package com.adaptris.rest;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Properties;
+import java.nio.file.Paths;
+import java.util.*;
 
 import com.adaptris.core.AdaptrisMessage;
 import com.adaptris.core.AdaptrisMessageListener;
@@ -10,11 +11,14 @@ import com.adaptris.core.CoreException;
 import com.adaptris.core.DefaultMessageFactory;
 import com.adaptris.core.DefaultSerializableMessageTranslator;
 import com.adaptris.core.management.MgmtComponentImpl;
+import com.adaptris.core.util.JmxHelper;
 import com.adaptris.core.util.LifecycleHelper;
 import com.adaptris.interlok.client.MessageTarget;
 import com.adaptris.interlok.client.jmx.InterlokJmxClient;
 import com.adaptris.interlok.types.SerializableMessage;
 import com.adaptris.util.stream.StreamUtil;
+
+import javax.management.*;
 
 public class WorkflowServicesComponent extends MgmtComponentImpl implements AdaptrisMessageListener {
   
@@ -50,8 +54,9 @@ public class WorkflowServicesComponent extends MgmtComponentImpl implements Adap
       
         AdaptrisMessage responseMessage = this.getMessageTranslator().translate(processedMessage);
         this.getConsumer().doResponse(message, responseMessage);
+        //Segment for Co-Din
       } else { // we'll just return the definition.
-        AdaptrisMessage responseMessage = generateDefinitionFile(); 
+        AdaptrisMessage responseMessage = generateDefinitionFile();
         this.getConsumer().doResponse(message, responseMessage);
       }
     } catch (Exception e) {
@@ -61,11 +66,47 @@ public class WorkflowServicesComponent extends MgmtComponentImpl implements Adap
       } catch (Exception silent) {}
     }
   }
+  //Segment for Co-Din
+  private AdaptrisMessage generateDefinitionFile() throws IOException, MalformedObjectNameException {
+    List<String> definition = new ArrayList<String>();
+//    String definition = null;
+    // build the string
+    // Selects
+    String objectName ="*com.adaptris:type=Workflow,*";
 
-  private AdaptrisMessage generateDefinitionFile() throws IOException {
-    InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream(DEFINITION_FILE);
+    MBeanServer interlokMBeanServer = JmxHelper.findMBeanServer();
+//    QueryExp exp = Query.eq(Query.attr("type"), Query.value("Workflow"));
+    Set<ObjectInstance> objectInstanceSet = interlokMBeanServer.queryMBeans(new ObjectName(objectName), null);
+    Iterator<ObjectInstance> iterator = objectInstanceSet.iterator();
+    while (iterator.hasNext()) {
+      ObjectInstance instance = iterator.next();
+      if (instance.getClassName() == "com.adaptris.core.runtime.WorkflowManager") {
+
+        System.out.println("****************************************");
+        System.out.println("Class Name:t" + instance.getClassName());
+        System.out.println("Object Name:t" + instance.getObjectName());
+        System.out.println("****************************************");
+
+//        definition = instance.getObjectName().getKeyPropertyList().toString();
+
+                definition.add(instance.getObjectName().getKeyPropertyListString());
+
+
+      }
+
+    }
+
     AdaptrisMessage responseMessage = DefaultMessageFactory.getDefaultInstance().newMessage();
-    StreamUtil.copyAndClose(resourceAsStream, responseMessage.getOutputStream());
+
+//    Line below for testing filter as string, one channel and workflow
+//    responseMessage.setContent(definition, responseMessage.getContentEncoding());
+
+//    Line below for testing final solution with multiple channels and workflows
+    responseMessage.setContent((Arrays.toString(definition.toArray())), responseMessage.getContentEncoding());
+    System.out.println("****************************************");
+    System.out.println(definition);
+    System.out.println("****************************************");
+
     return responseMessage;
   }
   
