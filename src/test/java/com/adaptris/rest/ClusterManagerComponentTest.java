6f0dc7b0ac06671a2c81397287a255f77d111d65
Aaron McGrath <aaron.mcgrath@adaptris.com>
2020-08-24 12:56:18 +0100

    INTERLOK-3407 A new workflow service that allows Prometheus to scrape metrics from our JMX container.  This is an alternative to using the Prometheus pushgateway.

--- src/test/java/com/adaptris/rest/ClusterManagerComponentTest.java
+++ src/test/java/com/adaptris/rest/ClusterManagerComponentTest.java
@@ -5,20 +5,22 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
+
 import java.util.List;
 import java.util.Properties;
 import java.util.UUID;
+
 import javax.management.MalformedObjectNameException;
+
 import org.awaitility.Durations;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
 import com.adaptris.core.AdaptrisMessage;
-import com.adaptris.core.AdaptrisMessageListener;
 import com.adaptris.core.DefaultMessageFactory;
-import com.adaptris.core.StandaloneConsumer;
 import com.adaptris.core.XStreamJsonMarshaller;
 import com.adaptris.core.cache.ExpiringMapCache;
 import com.adaptris.mgmt.cluster.ClusterInstance;
@@ -31,7 +33,7 @@ public class ClusterManagerComponentTest {
 
   private ClusterManagerComponent clusterManagerComponent;
 
-  private TestConsumer testConsumer = new TestConsumer();
+  private MockWorkflowConsumer testConsumer = new MockWorkflowConsumer();
 
   private ExpiringMapCache expiringMapCache;
 
@@ -47,7 +49,7 @@ public class ClusterManagerComponentTest {
 
   @Before
   public void setUp() throws Exception {
-    MockitoAnnotations.initMocks(this);
+    MockitoAnnotations.openMocks(this);
 
     message = DefaultMessageFactory.getDefaultInstance().newMessage();
 
@@ -151,40 +153,5 @@ public class ClusterManagerComponentTest {
       .until(testConsumer::complete);
 
     assertTrue(testConsumer.isError);
-  }
-
-  class TestConsumer extends WorkflowServicesConsumer {
-
-    String payload;
-    boolean isError;
-    int httpStatus = -1;
-
-    @Override
-    protected StandaloneConsumer configureConsumer(AdaptrisMessageListener messageListener, String consumedUrlPath, String acceptedHttpMethods) {
-      return null;
-    }
-
-    @Override
-    protected void doResponse(AdaptrisMessage originalMessage, AdaptrisMessage processedMessage,
-        String contentType, int status) {
-      payload = processedMessage.getContent();
-      httpStatus = status;
-    }
-
-
-    @Override
-    public void doErrorResponse(AdaptrisMessage message, Exception e, int status) {
-      isError = true;
-      httpStatus = status;
-    }
-
-    public boolean complete() {
-      return isError == true || payload != null;
-    }
-
-    @Override
-    public void prepare() {
-
-    }
   }
 }
